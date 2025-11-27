95c1215d938579596e3ade0371b9a000c79a9cd1
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-11-27 18:03:36 +0000

    .

---
+++ Pilha/Balanceados/script.js
@@ -0,0 +1,5 @@
+function verificar() {
+    let texto = document.getElementById('texto').value;
+    let saida = document.getElementById('resultado');
+    let pilha = new Stack();
+    let valido = true;
