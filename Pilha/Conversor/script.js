7bfdcc57c318c507ae4a3d914b48da625b8ac35b
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-11-27 01:03:58 +0000

    finalizando_conversor

--- Pilha/Conversor/script.js
+++ Pilha/Conversor/script.js
@@ -1,10 +1,22 @@
 function converter() {
+    //pega o valor do html
     let numero = parseInt(document.getElementById('numero').value);
+
     const pilha = new Stack();
     let resto;
-    let stringBinaria = '';
+    let binaryString = '';
 
-    if (!numero) {
-        alert("digite um numero valido");
-        return;
+    // faz o loop dividindo por 2 até o numero zerar
+    while (numero > 0) {
+        //pega o resto da divisao
+        resto = parseInt(numero % 2);
+        //guarda o resto na pilha empilha
+        pilha.push(resto);
+        //pega o número e divide por 2
+        numero = parseInt(numero / 2);
     }
+    while (!pilha.isEmpty()) {
+        binaryString += pilha.pop().toString();
+    }
+    document.getElementById('resultado').innerText = binaryString;
+}
